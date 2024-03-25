6028297659ba85c22fa3d723adbc826a6ffc5f49
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-03-25 17:30:11 +0100

    add: inputs

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// !INPUT
+
+//* km da percorrere
+let totalKm = prompt ("Ciao! Quanti km devi percorrere?"); //? string | null
+totalKm = parseInt (totalKm); //? from string to number
+console.log(totalKm, typeof totalKm);
+
+//* età del passeggero
+let passAge = prompt ("Ok! quanti anni hai?"); //? string | null
+passAge = parseInt (passAge); //? from string to number
+console.log(passAge, typeof passAge);
+
+// !LOGICA
